93571ba65802b3932f56a2d3255853ff6806eaa3
Abdullah Mahmoud <abdullahmahmoud289@gmail.com>
2023-03-16 22:06:59 +0200

    added Exceptions: GameAction, Invalid Target

---
+++ src/exceptions/GameActionException.java
@@ -0,0 +1,13 @@
+package exceptions;
+
+public abstract class GameActionException extends Exception {
+	
+	public GameActionException() {
+		super();
+	}
+	
+	public GameActionException(String s) {
+		super(s);
+	}
+
+}
