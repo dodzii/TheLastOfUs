3cce5610bef3b022d5435651b8786c36c17f1eed
Ali <amsaar.1416@gmail.com>
2023-03-20 12:23:52 +0200

    Added movment exceptio class

--- src/model/world/CharacterCell.java
+++ src/model/world/CharacterCell.java
@@ -11,20 +11,25 @@ public class CharacterCell extends Cell {
 		this.character = character;
 		this.isSafe = isSafe;
 	}
-	
 	//Getters & Setters
 	public Character getCharacter() {
 		return character;
 	}
-	public void setCharacter(Character charcter) {
-		this.character = charcter;
+
+	public void setCharacter(Character character) {
+		this.character = character;
 	}
+
 	public boolean isSafe() {
 		return isSafe;
 	}
+
 	public void setSafe(boolean isSafe) {
 		this.isSafe = isSafe;
 	}
+	
+	
+	
 	
 	
 	
