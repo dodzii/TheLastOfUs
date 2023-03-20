f40f13d2babb60962e1fa2e73b2d90a1133fe5ef
Talaat-jr <abdelrahmanmt96@gmail.com>
2023-03-20 13:59:01 +0200

    modified Charcter

--- src/model/characters/Character.java
+++ src/model/characters/Character.java
@@ -20,8 +20,6 @@ public abstract class Character {
 		this.maxHp = maxHp;
 		this.attackDmg = attackDmg;
 		this.currentHp=maxHp;
-		this.target=null;
-		this.location=null;
 	}
 	
 	//Getters & Setters
