85655228d101e284a97d09bb16cbd2f8b2da9e76
Abdullah Mahmoud <abdullahmahmoud289@gmail.com>
2023-05-03 20:41:57 +0300

    Finished endTurn method

--- src/engine/Game.java
+++ src/engine/Game.java
@@ -1,6 +1,10 @@
 package engine;
 
 import java.util.*;
+
+import exceptions.InvalidTargetException;
+import exceptions.NotEnoughActionsException;
+
 import java.io.*;
 import model.characters.*;
 import model.collectibles.*;
@@ -137,7 +141,7 @@ public class Game {
 		}
 		return((heroes.size()>=5));
 	}
-	public static void endTurn() {
+	public static void endTurn() throws InvalidTargetException, NotEnoughActionsException {
 		setAllInvisible();
 		for(int i=0;i<15;i++) {
 			for(int j=0;j<15;j++) {
@@ -148,6 +152,60 @@ public class Game {
 					h.setTarget(null);
 					h.setSpecialAction(false);;
 					h.assignVisibilityAround();
+				}
+			}
+		}
+		for(int i=0;i<15;i++) {
+			for(int j=0;j<15;j++) {
+				Cell cell = map[i][j];
+				if(cell instanceof CharacterCell && ((CharacterCell)cell).getCharacter() instanceof Zombie) {
+					Zombie z =(Zombie)((CharacterCell)cell).getCharacter();
+					int x = z.getLocation().x;
+					int y = z.getLocation().y;
+					int l = x-1;
+					int r = x+1;
+					int u = y+1;
+					int d = y-1;
+					if((u>=0&&u<=14)&& map[x][u] instanceof CharacterCell && ((CharacterCell)map[x][u]).getCharacter() instanceof Hero) {
+						Hero h = (Hero)((CharacterCell)map[x][u]).getCharacter();
+						z.setTarget(h);
+						z.attack();
+					}
+					else if((d>=0&&d<=14)&& map[x][d] instanceof CharacterCell && ((CharacterCell)map[x][d]).getCharacter() instanceof Hero) {
+						Hero h = (Hero)((CharacterCell)map[x][d]).getCharacter();
+						z.setTarget(h);
+						z.attack();
+					}
+					else if((l>=0&&l<=14)&& map[l][y] instanceof CharacterCell && ((CharacterCell)map[l][y]).getCharacter() instanceof Hero) {
+						Hero h = (Hero)((CharacterCell)map[l][y]).getCharacter();
+						z.setTarget(h);
+						z.attack();
+					}
+					else if((r>=0&&r<=14)&& map[r][y] instanceof CharacterCell && ((CharacterCell)map[r][y]).getCharacter() instanceof Hero) {
+						Hero h = (Hero)((CharacterCell)map[r][y]).getCharacter();
+						z.setTarget(h);
+						z.attack();
+					}
+					else if((u>=0&&u<=14)&&(l>=0&&l<=14)&& map[l][u] instanceof CharacterCell && ((CharacterCell)map[l][u]).getCharacter() instanceof Hero) {
+						Hero h = (Hero)((CharacterCell)map[l][u]).getCharacter();
+						z.setTarget(h);
+						z.attack();
+					}
+					else if((u>=0&&u<=14)&&(r>=0&&r<=14)&& map[r][u] instanceof CharacterCell && ((CharacterCell)map[r][u]).getCharacter() instanceof Hero) {
+						Hero h = (Hero)((CharacterCell)map[r][u]).getCharacter();
+						z.setTarget(h);
+						z.attack();
+					}
+					else if((d>=0&&d<=14)&&(l>=0&&l<=14)&& map[l][d] instanceof CharacterCell && ((CharacterCell)map[l][d]).getCharacter() instanceof Hero) {
+						Hero h = (Hero)((CharacterCell)map[l][d]).getCharacter();
+						z.setTarget(h);
+						z.attack();
+					}
+					else if((d>=0&&d<=14)&&(r>=0&&r<=14)&& map[r][d] instanceof CharacterCell && ((CharacterCell)map[r][d]).getCharacter() instanceof Hero) {
+						Hero h = (Hero)((CharacterCell)map[r][d]).getCharacter();
+						z.setTarget(h);
+						z.attack();
+					}
 				}
 			}
 		}
